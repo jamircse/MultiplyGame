3a2254970481c283b4fc84359b88e2f67619a07c
Jamir Hossain <46633915+jamircse@users.noreply.github.com>
2022-10-18 13:46:25 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -80,3 +80,45 @@ localStorage.setItem("step",JSON.stringify(step));
 localStorage.setItem("num1",JSON.stringify(num1));
 localStorage.setItem("num2",JSON.stringify(num2));
 showMassage(message,st);
+
+
+
+
+//  Celebrate Step up and down 
+var container = document.querySelector("body");
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (width - 200) + 100;
+    const y = Math.random() * (height - 200) + 100;
+//     console.log(x, y);
+    return {x,y}
+}
+
+function createHeard() {
+    var heardurl = document.createElement("span");
+    heardurl.classList.add('fa');
+    heardurl.classList.add('fa-heart');
+    const size = Math.ceil(Math.random() * 30);
+    heardurl.style.fontSize = `${size}px`;
+    const {x,y} = getRandomLocation();
+    heardurl.style.top = `${y}px`;
+    heardurl.style.left = `${x}px`;
+
+    container.appendChild(heardurl);
+    setTimeout(() => {
+        heardurl.remove();
+    }, 500);
+}
+
+
+function startCelebrate() {
+    intervalid = setInterval(createHeard, 5);
+}
+//    const intervalid=setInterval(createHeard,5);
+
+setTimeout(() => {
+    clearInterval(intervalid);
+}, 5000) 
+
+
